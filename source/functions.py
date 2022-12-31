24511b6b50edfc2903daea1a7d99750c0eeceb12
Young-Jo-Choi <a6672284@gmail.com>
2022-12-31 18:35:09 +0900

    2022-12-31

---
+++ source/functions.py
@@ -0,0 +1,160 @@
+import torch
+from torch import nn
+from torch.nn import functional as Flatten
+from torch import optim
+from torchvision import models
+import copy
+import time
+
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def get_model(model_name, pretrained=True, transfer=False, feature_extract=False, num_classes=None):
+    if model_name =='resnet':
+        model = models.resnet18(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            num_ftrs = model.fc.in_features
+            model.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    elif model_name == 'alexnet':
+        model = models.alexnet(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            num_ftrs = model.classifier[6].in_features
+            model.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    elif model_name == 'vgg':
+        model = models.vgg16(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            num_ftrs = model.classifier[6].in_features
+            model.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    elif model_name == 'squeezenet':
+        model = models.squeezenet1_0(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            model.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+            model.num_classes = num_classes
+        input_size = 224
+    elif model_name == 'densenet':
+        model = models.densenet121(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            num_ftrs = model.classifier.in_features
+            model.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    elif model_name == 'inception':
+        model = models.inception_v3(pretrained=pretrained)
+        set_parameter_requires_grad(model, feature_extract)
+        if transfer:
+            # Handle the auxilary net
+            num_ftrs = model.AuxLogits.fc.in_features
+            model.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+            # Handle the primary net
+            num_ftrs = model.fc.in_features
+            model.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 299
+    else:
+        raise ValueError('Specifiy your model name')
+    return model, input_size
+
+def get_model_parameters(model, feature_extract):
+    params_to_update = model.parameters()
+    print("Params to learn:")
+    if feature_extract:
+        params_to_update = []
+        for name,param in model.named_parameters():
+            if param.requires_grad == True:
+                params_to_update.append(param)
+                print("\t",name)
+    else:
+        for name,param in model.named_parameters():
+            if param.requires_grad == True:
+                print("\t",name)
+    return params_to_update
+
+def train_model(model, dataloaders, criterion, optimizer, device, num_epochs=25, is_inception=False):
+    model = model.to(device)
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
